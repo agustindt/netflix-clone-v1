2a7d58eb98b9033ae90e644dc18f2650829ca68c
Agustin Di Tomaso <agustindt95@gmail.com>
2024-02-22 03:17:52 -0300

    WF
    Finally

--- components/MovieList.tsx
+++ components/MovieList.tsx
@@ -1,28 +1,31 @@
-import React from 'react'
-import{ isEmpty} from 'lodash'
-import MovieCard from './MovieCard';
+import React from 'react';
 
+import { MovieInterface } from '@/types';
+import MovieCard from '@/components/MovieCard';
+import { isEmpty } from 'lodash';
 
-interface MovieListProps{
-  data: Record<string, any>[];
+interface MovieListProps {
+  data: MovieInterface[];
   title: string;
 }
 
+const MovieList: React.FC<MovieListProps> = ({ data, title }) => {
+  if (isEmpty(data)) {
+    return null;
+  }
 
-const MovieList:React.FC<MovieListProps> = ({data, title}) => {
-if (isEmpty(data)){
-  return null;
-}
   return (
-    <div className="px-4 md:px12 mt-4 space-y-8">
-      <h1 className='text-white text-md md:text-xl lg:text-2xl font-semibold' >{title}</h1>
-      <div className='grid grid-cols-4 gap-2'>
-        {data.map((movie) =>(
+    <div className="px-4 md:px-12 mt-4 space-y-8">
+      <div>
+        <p className="text-white text-md md:text-xl lg:text-2xl font-semibold mb-4">{title}</p>
+        <div className="grid grid-cols-4 gap-2">
+          {data.map((movie) => (
             <MovieCard key={movie.id} data={movie} />
-        ))}
+          ))}
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default MovieList
+export default MovieList;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,10 @@
 import Billboard from "@/components/Billboard";
+import InfoModal from "@/components/InfoModal";
 import MovieList from "@/components/MovieList";
 import Navbar from "@/components/Navbar";
 import useCurrentUser from "@/hooks/useCurrentUser";
 import useFavorites from "@/hooks/useFavorites";
+import useInfoModalStore from "@/hooks/useInfoModalStore";
 import useMovieList from "@/hooks/useMovieList";
 import { NextPageContext } from "next";
 import { getSession} from "next-auth/react";
@@ -26,8 +28,10 @@ export default function Home() {
 
 const {data: movies =[]} =useMovieList()
 const {data: favorites =[]} = useFavorites()
+const {isOpen, closeModal} = useInfoModalStore();
   return (
     <div>
+      <InfoModal visible={isOpen} onClose={closeModal} />
       <Navbar />
       <Billboard />
       <div className="pb-40">
